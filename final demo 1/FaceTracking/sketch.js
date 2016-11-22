ce7349f8623d711783fa40cd16a5d050151b485f
MahotsahaDas <nk1444@nyu.edu>
2016-11-22 12:00:14 -0500

    Add files via upload

---
+++ final demo 1/FaceTracking/sketch.js
@@ -0,0 +1,34 @@
+// https://github.com/kylemcdonald/AppropriatingNewTechnologies/wiki/Week-2
+var capture;
+var tracker
+var w = 840,
+  h = 580;
+
+var tv_image;
+
+function setup() {
+  capture = createCapture(VIDEO);
+  createCanvas(w, h);
+  capture.size(w, h);
+  capture.hide();
+
+  colorMode(HSB);
+
+  tracker = new clm.tracker();
+  tracker.init(pModel);
+  tracker.start(capture.elt);
+
+  tv_image = loadImage('Crownt.png');
+  er = loadImage('earring.png');
+}
+
+function draw() {
+  image(capture, 0, 0, w, h);
+  var positions = tracker.getCurrentPosition();
+ 
+  if (positions.length > 0) {
+    image(tv_image, positions[33][0] - 75, positions[33][1] - 320, 200, 240);
+    image(er, positions[33][0] + 45, positions[33][1] + 30, 120, 140);
+    image(er, positions[33][0] - 155, positions[33][1] + 30, 120, 140);
+  }
+}
